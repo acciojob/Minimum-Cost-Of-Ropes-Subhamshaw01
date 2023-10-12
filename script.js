de05bdd5c9ff1f0830e5b1856e605e3e67db5ac2
Subhamshaw01 <137093444+Subhamshaw01@users.noreply.github.com>
2023-10-12 20:23:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function calculateMinCost() {
+let sum=function calculateMinCost(arr) {
+	return function (a){
+		return function(b){
+			if(b){
+				return sum(a+b);
+			}
+			return a;
+		}
+	}
+	console.log(sum(arr));
   //your code here
   
   
